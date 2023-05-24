74fa4cf395807bbfc7f4e96a5efc121e865ddaa4
kty <xodud9625@hanmail.net>
2023-05-24 10:06:12 +0900

    secondday

---
+++ ifstatement.py
@@ -0,0 +1,21 @@
+# a = int(input('숫자를 입력하시오.'))
+# if a >10:
+#       print("a는 10보다 큽니다.")
+# else:
+#       print("a는 10보다 작습니다.")
+# print("a는 10보다 작습니다.")
+
+
+# trueOrFalse = True
+# if trueOrFalse:
+#     print("참입니다")
+# else:
+#     print("거짓입니다")
+
+cash = int(input("얼마를 가지고 있습니까?"))
+
+if cash >= 30000:
+    print("택시를 타고 가시오.")
+else:
+    print("걸어가시오.")
+
