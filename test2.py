3a71a5396f675d8ac3184c7ede801576cf0ef06c
kty <xodud9625@hanmail.net>
2023-06-01 18:00:38 +0900

    day 8

--- test2.py
+++ test2.py
@@ -23,25 +23,25 @@
 # print(dict1)
 
 
-participant = ["mislav", "stanko", 'mislav',"mislav", "ana"]	
-completion = ['stanko','mislav', 'ana','mislav']
+# participant = ["mislav", "stanko", 'mislav',"mislav", "ana"]	
+# completion = ['stanko','mislav', 'ana','mislav']
 
-#completion을 dict로 변환
-# participant를 for 문으로 1개씩 꺼내서 completion[p] = completion[p] -1
+# #completion을 dict로 변환
+# # participant를 for 문으로 1개씩 꺼내서 completion[p] = completion[p] -1
 
-dictc ={}
-for c in completion:
-  if c not in dictc.keys():
-      dictc[c] = 1  
-  else:
-     dictc[c] = dictc[c] + 1
-for p in participant:
-  if p in dictc.keys() and dictc[p] > 0:
-    dictc[p] = dictc[p] -1
-    print(p)
-  else:
-     answer = p
-print(answer)
+# dictc ={}
+# for c in completion:
+#   if c not in dictc.keys():
+#       dictc[c] = 1  
+#   else:
+#      dictc[c] = dictc[c] + 1
+# for p in participant:
+#   if p in dictc.keys() and dictc[p] > 0:
+#     dictc[p] = dictc[p] -1
+#     print(p)
+#   else:
+#      answer = p
+# print(answer)
    
       
 # answer = ''
@@ -52,12 +52,25 @@ print(answer)
 #         answer = a
 # return answer
 
+# 가운데 글자 뽑기
+# s = "qwer"
+# for a in s:
+#   if len(s) %2 == 1: 
+#      answer = s[len(s) // 2]    
+#   else:
+#       answer = s[int(len(s)//2)-1:int(len(s)//2)]
+# print(s[1:2])
+# print(answer)
 
-s = "qwer"
-for a in s:
-  if len(s) %2 == 1: 
-     answer = s[len(s) // 2]    
+arr = [4,3,2,1]
+templist = arr[:]
+answer = []
+min = arr[0]
+for a in range(len(arr)):
+  if min <= arr[a]:
+    templist.remove(arr[a])
   else:
-      answer = s[int(len(s)//2)-1:int(len(s)//2)]
-print(s[1:3])
+    min = arr[a]  
+  answer.append(min)
 print(answer)
+
